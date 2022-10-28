3d67c3889cb58101c98cd5a26c76382ca74b6c62
anajembaedwin <anajembaedwin@gmail.com>
2022-10-28 06:11:41 +0100

    updated server.js to include /home route

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const fs = require('fs');
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+  	const htmlFile = 'index.html';
+  	fs.stat(`./${htmlFile}`, (err, stats) => {
+  		if(stats) {
+  			res.statusCode = 200;
+  			res.setHeader('Content-Type', 'text/html');
+		  	fs.createReadStream(htmlFile).pipe(res);
+  		}
+  	});
+  }
+
+  if(req.url === '/home') {
+  	const htmlFile = 'index.html';
+  	fs.stat(`./${htmlFile}`, (err, stats) => {
+  		if(stats) {
+  			res.statusCode = 200;
+  			res.setHeader('Content-Type', 'text/html');
+		  	fs.createReadStream(htmlFile).pipe(res);
+  		}
+  	});
+  }
+
+  if(req.url.endsWith('.html')) {
+    const htmlFile = req.url.slice(1);
+    fs.stat(`./${htmlFile}`, (err, stats) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      if(stats) {
+        fs.createReadStream(htmlFile).pipe(res);
+      } else {
+      	res.statusCode = 404;
+        res.end('Sorry, page not found');
+      }
+    });
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
